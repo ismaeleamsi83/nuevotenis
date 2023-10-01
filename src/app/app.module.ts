36c02173f0a3e4446653fe7eb1d650c72e65f1b8
ismael <ismaelreyplata@gmail.com>
2023-10-01 13:13:22 +0200

    header falta que funcione el boton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { HeaderComponent } from './header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { JugadoresComponent } from './jugadores/jugadores.component';
+import { PartidosComponent } from './partidos/partidos.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    ContactoComponent,
+    HeaderComponent,
+    InicioComponent,
+    JugadoresComponent,
+    PartidosComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
