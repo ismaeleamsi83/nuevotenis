bfeceb9238a4898d897a58c77735e25ba0fc710c
ismael <ismaelreyplata@gmail.com>
2023-10-04 23:17:07 +0200

    Medio hecho jugadores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { JugadoresComponent } from './jugadores/jugadores.component';
 
 const routes: Routes = [
-  { path: '', component: InicioComponent }
+  { path: '', component: InicioComponent},
+  { path: 'jugadores', component: JugadoresComponent }
 ];
 
 @NgModule({
